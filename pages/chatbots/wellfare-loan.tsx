ac076e953492b66609bd764f7b5d40bc91ab191e
pornchai <chaweewat.p@gmail.com>
2025-05-26 09:41:13 +0700

    add loan

--- pages/chatbots/wellfare-loan.tsx
+++ pages/chatbots/wellfare-loan.tsx
@@ -4,7 +4,7 @@ import { useRouter } from "next/router";
 export default function WellfareLoanChatbot() {
   const router = useRouter();
   const iframeSrc =
-    "https://copilotstudio.microsoft.com/environments/Default-a2339f6d-bf4a-44db-88ec-cb8f27da4abb/bots/cr62c_fJmRVTpJbFEyVsro40Fp7/webchat?__version__=2";
+    "https://copilotstudio.microsoft.com/environments/Default-a2339f6d-bf4a-44db-88ec-cb8f27da4abb/bots/cr62c_agentABMWBY/webchat?__version__=2";
 
   return (
     <div className="p-4">
